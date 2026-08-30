05462ce5f0aeb378ce65225d338e8a36b1c1a4a0
sahadul <mdsahadulhaque@gmail.com>
2026-08-30 16:05:23 +0600

    phone updated

--- app/data/brand.ts
+++ app/data/brand.ts
@@ -2,7 +2,7 @@ export const BRAND = {
   name: "Rong Dhonu Renovation Limited",
   shortName: "Rong Dhonu",
   tagline: "COLOR | DESIGN | TRANSFORM",
-  phone: "+880 17XXX XXXXXX",
+  phone: "+880 1886 030025",
   email: "info@rongdhonu.com",
   website: "www.rongdhonu.com",
   address: "২৫৬/২, পশ্চিম আগারগাঁও, আগারগাঁও, ঢাকা ১২০৭, বাংলাদেশ",
